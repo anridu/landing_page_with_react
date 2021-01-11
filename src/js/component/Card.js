fc77364031ca9fff766b7be27f376097c18cebb0
anridu <angelriverod@gmail.com>
2021-01-11 20:36:42 +0000

    error no identificado

--- src/js/component/Card.js
+++ src/js/component/Card.js
@@ -1,20 +1,24 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 //create your first component
-export default function Card() {
+export default function Card(props) {
 	return (
-		<div classNames="card" style="width: 18rem;">
-			<img src="..." className="card-img-top" alt="..." />
+		<div className="card" style="width: 18rem;">
+			<div>
+				<img className="card-img-top" />
+			</div>
 			<div className="card-body">
-				<h5 className="card-title">Card title</h5>
+				<h5 className="card-title">{props.title}</h5>
 				<p className="card-text">
 					Some quick example text to build on the card title and make
-					up the bulk of the card's content.
+					up the bulk of the card content.
 				</p>
-				<a href="#" className="btn btn-primary">
-					Go somewhere
-				</a>
 			</div>
 		</div>
 	);
 }
+
+Card.propTypes = {
+	title: PropTypes.string
+};
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -3,20 +3,13 @@ import Navbar from "./Navbar"; //Function
 import Jumbotron from "./Jumbotron"; //Clase
 import Card from "./Card"; //Hook
 
-
-
 //create your first component
 export function Home() {
-    return (
-        <div className="text-center mt-5">
-
-            <Navbar
-
-            />
-            <Card
-
-            />
-
-        </div>
-    );
+	return (
+		<div>
+			<Navbar />
+			<Card title={"title 1"} />
+			<Jumbotron />
+		</div>
+	);
 }
